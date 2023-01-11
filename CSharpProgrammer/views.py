0bd79fd0fa00ac2084c755840162e312503c032e
ildar <Ildar626gtr@gamail.com>
2023-01-11 22:45:28 +0500

    Рабочий переход по ссылкам, подключены статические файлы

--- CSharpProgrammer/views.py
+++ CSharpProgrammer/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def main_page(request):
+    return render(request, 'CSharpProgrammer/main.html')
+
+
+def relevance(request):
+    return render(request, 'CSharpProgrammer/main.html')
+
+
+def geography(request):
+    return render(request, 'CSharpProgrammer/main.html')
+
+
+def skills(request):
+    return render(request, 'CSharpProgrammer/main.html')
+
+
+def recent_vacancies(request):
     return render(request, 'CSharpProgrammer/main.html')
