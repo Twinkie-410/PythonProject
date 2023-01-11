cfb3834606732fd90d98a04a093ba860bbfb64e1
ildar <Ildar626gtr@gamail.com>
2023-01-11 23:40:24 +0500

    Базовый шаблон, отдельные страницы под каждый раздел

--- CSharpProgrammer/views.py
+++ CSharpProgrammer/views.py
@@ -3,20 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_page(request):
-    return render(request, 'CSharpProgrammer/main.html')
+    return render(request, 'CSharpProgrammer/main.html', {'tittle': 'Главная страница'})
 
 
 def relevance(request):
-    return render(request, 'CSharpProgrammer/main.html')
+    return render(request, 'CSharpProgrammer/relevance.html', {'tittle': 'Востребованность'})
 
 
 def geography(request):
-    return render(request, 'CSharpProgrammer/main.html')
+    return render(request, 'CSharpProgrammer/geography.html', {'tittle': 'География'})
 
 
 def skills(request):
-    return render(request, 'CSharpProgrammer/main.html')
+    return render(request, 'CSharpProgrammer/skills.html', {'tittle': 'Навыки'})
 
 
 def recent_vacancies(request):
-    return render(request, 'CSharpProgrammer/main.html')
+    return render(request, 'CSharpProgrammer/recent_vacancies.html', {'tittle': 'Последние вакансии'})
