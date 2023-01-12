aa5dcb25f69bc965fc4d9138c4ed295e4889a7e2
ildar <Ildar626gtr@gamail.com>
2023-01-12 20:11:41 +0500

    Header и footer сайта

--- CSharpProgrammer/models.py
+++ CSharpProgrammer/models.py
@@ -5,7 +5,7 @@ class Profession(models.Model):
     name = models.CharField("Название", max_length=40)
     description = models.TextField("Описание")
 
-    def __str(self):
+    def __str__(self):
         return self.name
 
     class Meta:
